a1da144f5f80620c9e3cc4b2e81cc5ba3c5c20ba
Edgar Fuentes <fuentesej@gmail.com>
2017-02-28 09:35:10 -0400

    bash script useful for count jobs by country

---
+++ count_jobs.sh
@@ -0,0 +1,15 @@
+print_count(){
+    keyword=$1
+    url=$2
+    action=$3
+    country=$4
+    url_get="$url$action?q=$keyword&l="
+    count=`curl -L -s $url_get \
+                 | sed -r -n 's/.*searchCount">(.*)<\/div>.*/\1/p' \
+                 | sed -r 's/1[^0-9]+([1-9]|10)//;s/[^0-9]//g'`
+    printf "%14u %-20s\n" "$count" $country
+}
+export -f print_count
+cat indeed_countries.txt | xargs -L 1 -I {} -P 4 bash -c "print_count $1 {}" | sort -nr
+
+
