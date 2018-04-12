1a633ff13894d9f8f2b1d309c730ddc80b84cb3a
efe <edgar.fuentes@telefonica.com>
2018-04-12 15:41:53 -0400

    taking into account page updates

--- count_jobs.sh
+++ count_jobs.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
+keyword=${1:-}
 print_count(){
-    keyword=$1
-    url=$2
-    action=$3
-    country=$4
-    url_get="${url}${action}?q=${keyword}&l="
-    count=$(curl -L -s "$url_get" \
-           | sed -r -n 's/.*searchCount">(.*)<\/div>.*/\1/p' \
+    local keyword=$1
+    local url=$2
+    local action=$3
+    local country=$4
+    local url_get="${url}${action}?q=${keyword}&l="
+    local count=$(curl -L -s "$url_get" \
+           | tr '\n' '\r'\
+           | sed -r -n -n 's/.*searchCount">[\r ]*([^<]+)<\/div>.*/\1/p'\
 	   | sed -r 's/1[^0-9]+([1-9]|10)//;s/[^0-9]//g')
     printf "%14u %-20s\n" "$count" "$country"
 }
 export -f print_count
-xargs -L 1 -I {} -P 4 bash -c "print_count $1 {}" < indeed_countries.txt | sort -nr
+xargs -L 1 -I {} -P 4 bash -c "print_count ${keyword} {}" < indeed_countries.txt | sort -nr
